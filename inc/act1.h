ab565575646b9809fffdb0edfe7e9a6cf1c8e057
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2021-09-16 21:16:30 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -5,28 +5,14 @@
 
 #include <avr/io.h>
 
-/**
- * @brief A macro to check if the button is pressed or not.
- *
- */
 #define SENSOR_ON !(PIND&(1<<PD4))
 
-/**
- * @brief A macro to check if the heater is pressed or not.
- *
- */
+
 #define HEATER_ON !(PIND&(1<<PD0))
 
-/**
- * @brief A macro to turn on the LED
- *
- */
+
 #define SET_LED PORTD|=(1<<PD2)
 
-/**
- * @brief A macro to turn off the LED
- *
- */
 #define CLEAR_LED PORTD&=~(1<<PD2)
 
 void Buttons_LED_Init();
