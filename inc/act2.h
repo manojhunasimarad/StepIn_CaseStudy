d552abb3fc2c970e7e2faf820e18405439cf5ff8
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2021-09-16 21:16:52 +0530

    Update act2.h

--- inc/act2.h
+++ inc/act2.h
@@ -3,16 +3,8 @@
 
 #include<avr/io.h>
 
-/**
- * @brief to read the adc values
- *
- */
 uint16_t ReadADC(uint8_t ch);
 
-/**
- * @brief initialise the analog to digital converter
- *
- */
 void InitADC();
 
 #endif // ACT2_H_INCLUDED
