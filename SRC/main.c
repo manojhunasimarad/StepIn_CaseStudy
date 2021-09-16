c47cdf99cf4bc737ae7ebfe240f0ae0e24be9284
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2021-09-16 21:15:07 +0530

    Update main.c

--- SRC/main.c
+++ SRC/main.c
@@ -1,4 +1,13 @@
-
+/**
+ * @file act1.c
+ * @author Manoj Hunasimarad
+ * @brief Activity
+ * @version 0.1
+ * @date 2021-09-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #include <util/delay.h>
 #include"lcd.h"
