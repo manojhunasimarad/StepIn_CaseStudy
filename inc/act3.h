d642d39df6067f680069f260e7f9447acc83656c
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2021-09-16 21:18:14 +0530

    Update act3.h

--- inc/act3.h
+++ inc/act3.h
@@ -1,38 +1,27 @@
+/**
+ * @file act1.c
+ * @author Manoj Hunasimarad
+ * @brief Activity 3
+ * @version 0.1
+ * @date 2021-09-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #ifndef __ACTIVITY_3_H__
 #define __ACTIVITY_3_H__
-/**
- * @file Activity_3.h
- * @author Vinayak K(vinukaralatti@gmail.com)
- * @brief This is the Header File for Activity_3
- * @version 0.1
- * @date 2021-04-29
- *
- * @copyright Copyright (c) 2021
- *
- */
 
-/**
- * Macro Definitions
- */
-#define PWM_20_PERCENT (205) /**< This is for Output PWM for 20% duty cycle */
-#define PWM_40_PERCENT (410) /**< This is for Output PWM for 40% duty cycle */
-#define PWM_70_PERCENT (717) /**< This is for Output PWM for 70% duty cycle */
-#define PWM_95_PERCENT (973) /**< This is for Output PWM for 95% duty cycle */
+#define PWM_20_PERCENT (205) /**< This will bring PWM for 20% duty cycle */
+#define PWM_40_PERCENT (410) /**< This will bring PWM for 40% duty cycle */
+#define PWM_70_PERCENT (717) /**< This will bring PWMWM for 70% duty cycle */
+#define PWM_95_PERCENT (973) /**< This will bring PWM for 95% duty cycle */
 
-/**
- * Include files
- */
+
 #include <util/delay.h>
 #include <avr/io.h>
 
-/**
- * Function Definitions
- */
 
-/**
- * @brief Function to Initialize Timer1 registers
- *
- */
 void Init_Timer();
 
 /**
