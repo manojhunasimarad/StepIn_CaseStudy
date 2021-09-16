d39eacf56ef46a0d3fb4ec4eedf9e970429fe5f6
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2021-09-16 21:14:31 +0530

    Update fuse.c

--- SRC/fuse.c
+++ SRC/fuse.c
@@ -1,3 +1,14 @@
+/**
+ * @file act1.c
+ * @author Manoj Hunasimarad
+ * @brief Activity
+ * @version 0.1
+ * @date 2021-09-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include <avr/io.h>
 
 FUSES = {
